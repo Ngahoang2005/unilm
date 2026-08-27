804ed2790bb76483cf6d93094a4f6cefd50c9111
thuwng <122499901+thuwng@users.noreply.github.com>
2026-08-27 17:16:38 +0700

    Update __init__.py

--- layoutlmv3/layoutlmft/models/layoutlmv3/__init__.py
+++ layoutlmv3/layoutlmft/models/layoutlmv3/__init__.py
@@ -13,12 +13,15 @@ from .tokenization_layoutlmv3 import LayoutLMv3Tokenizer
 from .tokenization_layoutlmv3_fast import LayoutLMv3TokenizerFast
 
 
-AutoConfig.register("layoutlmv3", LayoutLMv3Config)
-AutoModel.register(LayoutLMv3Config, LayoutLMv3Model)
-AutoModelForTokenClassification.register(LayoutLMv3Config, LayoutLMv3ForTokenClassification)
-AutoModelForQuestionAnswering.register(LayoutLMv3Config, LayoutLMv3ForQuestionAnswering)
-AutoModelForSequenceClassification.register(LayoutLMv3Config, LayoutLMv3ForSequenceClassification)
+AutoConfig.register("layoutlmv3", LayoutLMv3Config, exist_ok=True)
+AutoModel.register(LayoutLMv3Config, LayoutLMv3Model, exist_ok=True)
+AutoModelForTokenClassification.register(LayoutLMv3Config, LayoutLMv3ForTokenClassification, exist_ok=True)
+AutoModelForQuestionAnswering.register(LayoutLMv3Config, LayoutLMv3ForQuestionAnswering, exist_ok=True)
+AutoModelForSequenceClassification.register(LayoutLMv3Config, LayoutLMv3ForSequenceClassification, exist_ok=True)
 AutoTokenizer.register(
-    LayoutLMv3Config, slow_tokenizer_class=LayoutLMv3Tokenizer, fast_tokenizer_class=LayoutLMv3TokenizerFast
+    LayoutLMv3Config, 
+    slow_tokenizer_class=LayoutLMv3Tokenizer, 
+    fast_tokenizer_class=LayoutLMv3TokenizerFast,
+    exist_ok=True
 )
 SLOW_TO_FAST_CONVERTERS.update({"LayoutLMv3Tokenizer": RobertaConverter})
